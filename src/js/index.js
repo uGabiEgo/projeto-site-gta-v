b4fdc29f3f8c75f9447d291007c1737e835dd75f
Gabriel Oliveira <157628063+uGabiEgo@users.noreply.github.com>
2024-01-31 17:21:54 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+const botao = document.querySelector(".btn-plataformas");
+const elementoPlataformas = document.querySelector(".btn-plataformas .plataformas");
+
+botao.addEventListener("click", () => {
+
+    elementoPlataformas.classList.toggle("ativo");
+});
